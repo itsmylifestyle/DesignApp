0a786ee3f947a9ae6ccbcf5f740742826fc7e6f8
itsmylifestyle <hahaii00@mail.ru>
2022-10-21 14:50:58 +0600

    Initial Commit

---
+++ Design/DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignApp.swift
+//  Design
+//
+//  Created by Айбек on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
