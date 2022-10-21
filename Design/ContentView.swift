2ca4d58bdaabba34500732b5a5b2c91f7d171861
itsmylifestyle <hahaii00@mail.ru>
2022-10-21 14:54:57 +0600

    SwiftUI Stacks

--- Design/ContentView.swift
+++ Design/ContentView.swift
@@ -9,10 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("maptest")
+            Text("Eiffel Tower")
+                .font(.largeTitle)
+                .background(Color.white)
+                .foregroundColor(Color.red)
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
